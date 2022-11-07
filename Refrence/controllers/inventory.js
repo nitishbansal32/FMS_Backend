9c3cb59c56c75ec2da965b905d3ed081019816bf
Mohammad Zeya Ahmad <mohammadzeyaahmad@gmail.com>
2022-11-07 06:29:11 +0530

    unnecessary folder named Refrence removed

--- Refrence/controllers/inventory.js
+++
@@ -1,9 +0,0 @@
-const {Tractor}=require('../models/Tracktor_Inventory');
-const createInventory = async (req, res) => {
-    
-    const entry = await Tractor.create(req.body);
-    await entry.save();
-    res.json({ tractor:entry});
-  };
-  
-  module.exports=createInventory;
