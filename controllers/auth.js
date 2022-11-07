8c11300c9b7267275baa6b2df01d3f080194e401
Mohammad Zeya Ahmad <mohammadzeyaahmad@gmail.com>
2022-11-07 06:25:46 +0530

    little correction in the cookie expiration time for logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -72,7 +72,7 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
   res.cookie('token', 'logout', {
     httpOnly: true,
-    expires: new Date(Date.now()),
+    expires: new Date(Date.now() + 1000),
   });
   res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
 };
