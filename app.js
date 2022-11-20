e2b9cc238b9dba4826fa8fa9c7aeac3d794f210d
Mohammad Zeya Ahmad <mohammadzeyaahmad@gmail.com>
2022-11-20 23:10:29 +0530

    cors credentials set to true

--- app.js
+++ app.js
@@ -33,11 +33,7 @@ app.use(cors({
     origin: '*',
     credentials: true
 }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 app.use(xss());
 app.use(mongoSanitize());
 
