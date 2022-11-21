079cc420902b7a2d06f96b31f909e0347d6c24ee
Mohammad Zeya Ahmad <mohammadzeyaahmad@gmail.com>
2022-11-21 16:09:02 +0530

    small bug fixed

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,7 +3,7 @@ const CustomError = require('../errors');
 const { isTokenValid } = require('../utils');
 
 const authenticateUser = async (req, res, next) => {
-  const token = req.signedCookies.token;
+  let token = req.signedCookies.token;
 
   console.log(req.signedCookies.token)
   
