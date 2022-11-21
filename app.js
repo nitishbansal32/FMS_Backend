9763cd6bce2079ad259aeb59947e3290aa7ec55d
Mohammad Zeya Ahmad <mohammadzeyaahmad@gmail.com>
2022-11-21 08:58:08 +0530

    "little bug fixed"

--- app.js
+++ app.js
@@ -40,10 +40,7 @@ app.use((req, res, next) => {
       // if (allowedOrigins.indexOf(origin) > -1) {
       res.setHeader("Access-Control-Allow-Origin", origin);
       //}
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization, Content-Length, X-Requested-With"
-      );
+    
       res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
       res.header("X-XSS-Protection", "1; mode=block");
       res.header("Strict-Transport-Security", "max-age=31536000");
