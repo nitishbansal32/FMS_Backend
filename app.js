8b8491b7b770cdc2c7e9dcdeb7f18bedd76db062
Mohammad Zeya Ahmad <mohammadzeyaahmad@gmail.com>
2022-11-27 03:52:22 +0530

    small bug fixed

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
       let origin = req.headers.origin;
      //   origin = 'http://localhost:3000/'
       // if (allowedOrigins.indexOf(origin) > -1) {
-     res.setHeader("Access-Control-Allow-Origin", origin);
+     //res.setHeader("Access-Control-Allow-Origin", origin);
       
     
       res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE","OPTIONS");
